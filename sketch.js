2e234abc6842ec167840ac33ff20fda3e43f6861
Shreya251006 <73515288+Shreya251006@users.noreply.github.com>
2020-12-17 19:42:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = new Ground(width/2,height,width,10);
+	dustbin = new Dustbin(700,650,100,100);
+	paper = new Paper(50,630,20);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  Engine.update(engine);
+  background("lightblue");
+  ground.display();
+  dustbin.display();
+  paper.display();
+  drawSprites();
+ 
+}
+
+
+function keyPressed() { if (keyCode === UP_ARROW) { Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85}); } }
+
